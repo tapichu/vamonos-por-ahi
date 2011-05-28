7f5189b7b20d476bd8d6ff0246d866639011c985
Eduardo Lopez <eduardo.biagi@gmail.com>
2011-05-27 20:50:22 -0500

    Add some silly raphael fonts, animate them a bit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function($, undefined) {
+(function($, Raphael, undefined) {
 
   var slideSwitch = function() {
     $('div.slideshow').each(function() {
@@ -21,8 +21,34 @@
     });
   };
 
+  var fontAnimation = function(set) {
+    for (var i = 0, ii = set.length; i < ii; i++) {
+      set[i].animate({fill: Raphael.getColor()}, 3000, function() {
+        set.animate({rotation: 360}, 1000);
+      });
+    }
+  };
+
   $(document).ready(function() {
+    // Animate slideshows
     setInterval(slideSwitch, 5000);
+
+    var p1 = Raphael('title1', 560, 50);
+    var p2 = Raphael('title2', 560, 50);
+    var p3 = Raphael('title3', 560, 50);
+
+    var font = p1.getFont('whoa');
+
+    var slp = p1.print(5, 30, 'San Luis Potosi', font, 30)
+        .attr({fill: '#fff'});
+    var scdlc = p2.print(5, 30, 'San Cristobal de las Casas', font, 30)
+        .attr({fill: '#fff'});
+    var taxco = p3.print(5, 30, 'Taxco', font, 30)
+        .attr({fill: '#fff'});
+
+    fontAnimation(slp);
+    fontAnimation(scdlc);
+    fontAnimation(taxco);
   });
 
-})(jQuery);
+})(jQuery, Raphael);
